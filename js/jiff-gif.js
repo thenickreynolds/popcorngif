9966d4be9de9955eaa1f9694d319bb233908a111
Justin Frey <justinfrey64@gmail.com>
2019-11-26 16:46:40 -0500

    fixing anchor tags at bottom to open url in new tab

--- js/jiff-gif.js
+++ js/jiff-gif.js
@@ -51,6 +51,12 @@ JiffGif.setup = function(r) {
             var checked = $(this).prop('checked');
             toggleDarkMode(checked);
         });
+
+        r.on('click', 'a[target="_blank"]', function(e){
+            e.preventDefault();
+            chrome.tabs.create({url: $(this).prop('href'), active: false});
+            return false;
+        });
         
         setState(State.Intro);
         var jiffThemeSetting;
