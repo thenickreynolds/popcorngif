ae66b7201ea1c924775a3bdf4a548768f24d3854
Nick Reynolds <me@nickreynolds.net>
2020-07-31 16:05:45 -0700

    Add upsells to landing page

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "pure-react-carousel/dist/react-carousel.es.css";
 import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
