951409ee29d79bf8c383e07f320f8933caa3bfbe
Nick Reynolds <me@nickreynolds.net>
2020-07-31 12:52:41 -0700

    Tweak search icons, add back button to search

--- app/components/searchBox.tsx
+++ app/components/searchBox.tsx
@@ -18,9 +18,22 @@ export default function SearchBox({
     }
   }, [ref]);
 
+  const hasContent = text.length > 0;
+
   return (
     <div>
       <style jsx={true}>{`
+        .back_button {
+          position: absolute;
+          visibility: ${hasContent ? "visible" : "collapse"};
+          opacity: ${hasContent ? 1 : 0};
+          transition: ease-in-out, 0.35s ease-in-out;
+          width: 40px;
+          height: 45px;
+          top: 6px;
+          left: 8px;
+        }
+
         .search_input {
           width: 100%;
           outline: none;
@@ -31,14 +44,17 @@ export default function SearchBox({
           box-shadow: 2px 1px 3px gray;
           font-size: 12pt;
           font-weight: none;
-          background: #ee6e73 url(/search_icon_color_feature.svg) no-repeat
-            scroll 20px 22px;
+          background: #ee6e73 url(/icons/search_icon_color_feature.svg) no-repeat
+            scroll 20px 20px;
         }
 
         .search_input:focus {
           color: #000000;
-          background: #ffffff url(/search_icon_color_black.svg) no-repeat scroll
-            20px 22px;
+          background: #ffffff url(${
+            hasContent
+              ? "/icons/keyboard_arrow_left-24px.svg"
+              : "/icons/search_icon_color_black.svg"
+          }) no-repeat scroll 20px 20px};
         }
 
         .search_input:focus ::placeholder {
@@ -54,6 +70,19 @@ export default function SearchBox({
         }
       `}</style>
       <div>
+        <a
+          className="back_button"
+          href="#"
+          onClick={(e) => {
+            onChange("");
+            e.preventDefault();
+            if (ref.current) {
+              // focus and click required to trigger keyboard on mobile
+              ref.current.focus();
+              ref.current.click();
+            }
+          }}
+        />
         <input
           className="search_input"
           placeholder="Search gifs"
