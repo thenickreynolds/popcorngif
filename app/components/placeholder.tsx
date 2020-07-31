904df7ee400fb39e08e43eec692d842461a53653
Nick Reynolds <me@nickreynolds.net>
2020-07-31 14:37:49 -0700

    Tweak title text font weight

--- app/components/placeholder.tsx
+++ app/components/placeholder.tsx
@@ -14,6 +14,7 @@ export default function Placeholder({ onClick }: { onClick: () => void }) {
         }
 
         h1 {
+          font-size: 1.536rem;
           font-weight: normal;
         }
       `}</style>
