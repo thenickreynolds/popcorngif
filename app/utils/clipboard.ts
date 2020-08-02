eb7634a7cb49d92df2b56118ea4212a71b4fac46
Nick Reynolds <me@nickreynolds.net>
2020-08-02 09:11:40 -0700

    Switch clipboard technique to see if it fixes extension

--- app/utils/clipboard.ts
+++ app/utils/clipboard.ts
@@ -1,5 +1,13 @@
 export default class Clipboard {
   static write(text: string) {
-    navigator.clipboard.writeText(text);
+    const el = document.createElement("textarea");
+    el.value = text;
+    el.setAttribute("readonly", "");
+    el.style.position = "absolute";
+    el.style.left = "-9999px";
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
   }
 }
