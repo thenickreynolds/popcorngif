769aa9fcd4f2cae4e7752829d90dd7197460f685
Nick Reynolds <me@nickreynolds.net>
2020-08-06 15:55:29 -0700

    Tweak analytics path logging

--- app/components/analyticsPageLogger.tsx
+++ app/components/analyticsPageLogger.tsx
@@ -3,7 +3,8 @@ import { useEffect } from "react";
 
 export default function AnalyticsPageLogger() {
   useEffect(() => {
-    GALogger.page(window.location.href, document.title);
+    const path = window.location.pathname + window.location.search;
+    GALogger.page(path, document.title);
 
     // TODO potentially add facebook pixel here, main FB pixel library doesn't like SSR
   }, []);
